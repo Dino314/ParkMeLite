9e9cd3418e5b47606b25c601a5d4157a6c9dd267
Dino <dino.talundzic.sts@gmail.com>
2019-04-03 14:48:29 +0200

    Updated time checking

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+$(document).ready(function() {
+	
+});
+
 function prostCas(value, row, index){				
 	var today = new Date();				
-	var ura = today.getHours();				
+	var ura = today.getHours();
+	//ura = 20;
 	var danes = today.getDay();
+	//danes = 0;
 	var minute = 60-today.getMinutes();
-	console.log(minute);
+	var text = "";
 	
 	var teden = new Array(7);				
 	teden[0] = "Ned";				
@@ -22,16 +28,51 @@ function prostCas(value, row, index){
 	//console.log(dan.slice(0, 2));
 	//console.log(maxUra);
 	if (row.Type == "1" || (String(row.Type).charAt(5) == "0" && (mesec >= 10 || mesec <= 4)) || (String(row.Type).charAt(5) == "1" && (mesec >= 5 && mesec <= 9))){
-		if (ura > maxUra){
+		if (dan == "0"){
+			var i = danes+1;
+			var ostanek = 23-ura;
+			
+			while (row[teden[i%7]] == "0"){
+				ostanek += 24;
+				i++;
+			}
+			
+			ostanek += parseInt((row[teden[i%7]]+1).slice(0, 2));
+			
+			text += "<t class='text-success'>Brezplačno še ";
+			if (ostanek != 0){
+				text += "<b>"+ostanek+"ur</b> in ";
+			}
+			text += "<b>"+minute+" minut</b></t><br />";
+			return text;
+		}else if (ura > maxUra){
 			var ostanek = (parseInt(minUra)+23)-parseInt(ura);
 			//console.log("ostanek: "+ostanek);
-			return "<b class='text-success'>Brezplačno še "+ostanek+" ur in "+minute+" minut</b><br />"+dan;
+			
+			text += "<t class='text-success'>Brezplačno še ";
+			if (ostanek != 0){
+				text += "<b>"+ostanek+"ur</b> in ";
+			}
+			text += "<b>"+minute+" minut</b></t><br />";
+			return text;
 		}else if (ura < minUra){
 			var ostanek = parseInt(minUra)-parseInt(ura)-1;
-			return "<b class='text-success'>Brezplačno še "+ostanek+" ur in "+minute+" minut</b><br />"+dan;		
+			
+			text += "<t class='text-success'>Brezplačno še ";
+			if (ostanek != 0){
+				text += "<b>"+ostanek+"ur</b> in ";
+			}
+			text += "<b>"+minute+" minut</b></t><br />";
+			return text;
 		}else if(maxUra < 24){
-			var ostanek = parseInt(maxUra) - parseInt(ura);
-			return "<b class='text-danger'>Za plačat ostane še "+ostanek+"ur in "+minute+" minut</b><br />"+dan;
+			var ostanek = parseInt(maxUra) - parseInt(ura)-1;
+			
+			text += "<t class='text-danger'>Za plačat ostane še ";
+			if (ostanek != 0){
+				text += "<b>"+ostanek+"ur</b> in ";
+			}
+			text += "<b>"+minute+" minut</b></t><br />";
+			return text;
 		}else{
 			var i = danes+1;
 			var ostanek = 23-ura;
@@ -41,12 +82,23 @@ function prostCas(value, row, index){
 				i++;
 			}
 
-			return "<b class='text-danger'>Za plačat ostane še "+ostanek+"ur in "+minute+" minut</b><br />"+dan;
+			text += "<t class='text-danger'>Za plačat ostane še ";
+			if (ostanek != 0){
+				text += "<b>"+ostanek+"ur</b> in ";
+			}
+			text += "<b>"+minute+" minut</b></t><br />";
+			return text;
 		}
 	}else if (row.Type == "2"){
 		//za plačat 1€ na dan, ki traja samo do 23:59
 		var ostanek = 23-ura;
-		return "<b class='text-danger'>Za plačat 1€, ki traja do konca dneva; trenutno ostane še "+ostanek+" ur in "+minute+" minut</b><br />"+dan;
+		
+		text += "<t class='text-danger'>Za plačat 1€, ki traja do konca dneva; trenutno ostane še ";
+		if (ostanek != 0){
+			text += "<b>"+ostanek+"ur</b> in ";
+		}
+		text += "<b>"+minute+" minut</b></t><br />";
+		return text;
 
 	}else if (row.Type == "3"){
 		if (dan == "0"){
@@ -54,15 +106,21 @@ function prostCas(value, row, index){
 			if (danes == "6"){
 				 ostanek += 24;
 			}
-			return "<b class='text-success'>Brezplačno še "+ostanek+" ur in "+minute+" minut</b><br />"+dan;
+			
+			text += "<t class='text-success'>Brezplačno še ";
+			if (ostanek != 0){
+				text += "<b>"+ostanek+"ur</b> in ";
+			}
+			text += "<b>"+minute+" minut</b></t><br />";
+			return text;
 		}else{
-			return "<b class='text-danger'>Za plačat 1€ za 24ur</b><br />"+dan;
+			return "<b class='text-danger'>Za plačat 1€ za 24ur,<br />sobota in nedelja brezplačno</b><br />";
 		}
 	}else if (row.Type == "4"){
-		return "<b class='text-danger'>Vedno je za plačat!</b><br />"+dan;
+		return "<b class='text-danger'>Vedno je za plačat!</b><br />";
 	}
 	else{
-		return "<b class='text-warning'>Ne obratuje trenutno</b><br />"+dan;
+		return "<b class='text-warning'>Ne obratuje trenutno</b><br />";
 		/*$('#table').bootstrapTable('remove', {
 	        field: 'Parking',
 	        values: ["6"]
@@ -85,4 +143,8 @@ function cena(value){
 	}else{				
 		return value+"€";				
 	}			
+}
+
+function cenePodatki(value, row){
+	return "<b>Prva ura:</b> "+cena(row.PrvaUra)+"<br /><b>Vsaka naslednja:</b> "+cena(row.NovaUra);
 }
